f1ececd8f87952a36aefbaa45f2824d1b9e0684f
Rudy Cahyadi <rudy.cahyadi@mocogawe.com>
2021-07-11 19:31:22 +0800

    ad file

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(
     license='GNU GPLv3',
     packages=find_packages(exclude=['tests']),
     namespace_packages=['ckanext', 'ckanext.nhm'],
+    package_data={b'multigtfs': ['src/tdwg_dwcterms.xsd']},
     include_package_data=True,
     zip_safe=False,
     install_requires=[
